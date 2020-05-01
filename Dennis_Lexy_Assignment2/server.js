a261825952752d70f6570364ac3fc885f47123b3
Lexy <dennisal@hawaii.edu>
2020-04-30 22:18:20 -1000

    Assignment 2

--- Dennis_Lexy_Assignment2/server.js
+++ Dennis_Lexy_Assignment2/server.js
@@ -11,10 +11,9 @@ var express = require('express'); //load and cache express module
 var app = express(); //set module to variable 'app'
 var myParser = require("body-parser"); //load and cache body parser module
 var fs = require('fs'); // load and cache fs module
-var user_info_file = './public/user_data.json'; // set the .json file to the variable 'user_info_file'
+var user_info_file = './user_data.json'; // set the .json file to the variable 'user_info_file'
 var userdata = fs.readFileSync(user_info_file, 'utf-8'); //open file user_data.json and assign it (in a string) to var userdata
 userdata = JSON.parse(userdata); //json parse will convert string into json object
-
 
 app.all('*', function (request, response, next) { //for all request methods...
     console.log(request.method + ' to ' + request.path); //write in the console the request method and its path
@@ -25,6 +24,7 @@ app.use(myParser.urlencoded({ extended: true })); //get data in the body
 
 app.post("/process_form", function (request, response) { //process the quantity_form when the POST request is initiated to form a response from the values in the form
     let POST = request.body; // data would be packaged in the body
+    console.log(POST);
 
     if (typeof POST['purchase_submit_button'] != 'undefined') { //if the POST request is not undefined...
         var validAmount = true; // creating a variable 'validAmount' and assuming it will be true
@@ -68,26 +68,31 @@ function isNonNegInt(q, return_errors = false) {
 
 //The following code is taken from Lab 14 Exercise 3
 app.post("/check_login", function (request, response) {// Process login form POST and redirect to checkout if information in login page matches that in the login JSON file, back to login page if not
-    var err_str = ""; //assume there are no errors at first
+    errs = []; //assume no errors at first
     var login_username = request.body["username"]; //set var login_username to the username field in login page
-    
+
+
     if (typeof userdata[login_username] != 'undefined') { // If the username is not undefined...
         var user_info = userdata[login_username]; //set variable 
-        
-        if (user_info.password != request.body["password"]) { //check if username exists in json data already. If so, check if password matches
-            err_str = `bad_password`; //If they do not match, let user know the password is wrong
+
+        if (user_info.password != request.body["password"]) { //check if username exists in json data already
+            errs.push("Incorrect Password"); //return error message if password is wrong
         } else { //If the password matches...
-            //const successful_login_stringified = queryString.stringify(request.query); //converts the data to a string, adds it to the previous query string, and sets it to variable 'stringified'
-            response.redirect('./invoice.html?'+ "window.location.search"); // redirect the page to the login page with the stringified path in the query string
+            console.log(request.query);
+            request.query.username = login_username;
+            request.query.name = user_info.name;
+            const userdata_stringified = queryString.stringify(request.query); //converts the data to a string, adds it to the previous query string, and sets it to variable 'stringified'
+            response.redirect('./invoice.html?' + userdata_stringified); // redirect the page to the login page with the stringified path in the query string
             //response.redirect(`./invoice.html? + ${JSON.stringify(request.query)}`); //Redirect user to a checkout that informs them that *username* has logged in successfully
+            console.log(userdata_stringified);
             return; //stops function
         }
 
     } else {
-        err_str = `bad_username`; //If the username does not match, it is will return this message 
+        errs.push("Incorrect Username"); //If the username does not match, it is will return this message 
     }
 
-    response.redirect('./login.html?${username}=${login_username}&error=%{err_str}'); //redirect the user to the login page to re-enter his/her login info with the error message in the query string
+    response.redirect(`./login.html?username=${login_username}&error=%{errs.push} + ${queryString.stringify(request.query)}`); //redirect the user to the login page to re-enter his/her login info with the error message in the query string, but keeping the data from quantity_form
 
 });
 
@@ -96,29 +101,33 @@ app.post("/register_user", function (request, response) {
     // process a simple register form
     username = request.body.username; //set var username to the username inputted by user
     errs = []; //assume no errors at first
-    
+
     if (typeof userdata[username] != 'undefined') { //check if username is taken
         errs.push("username taken"); //return error message if username is taken
-    } else {
-        userdata[username] = {}; // Otherwise, input username into the json file for storage
     }
-    
+    if (username == '') {
+        errs.push("Please enter a username");
+    }
     if (request["body"]["password"] != request["body"]["repeat_password"]) {//Check if password is same as the repeat password field
         errs.push("passwords don't match"); // let user know if passwords do not match
-    } else {
-        userdata[username].password = request["body"]["password"]; //otherwise, set password in json file to the inputted one
     }
+    if (request.body.password == '') {
+        errs.push("Please enter a username");
+    }
+    if (request.body.email == '') {
+        errs.push("Please enter an email address");
+    }
+    if (errs.length == 0) {
+        //set the below variables to what was input by the user on the page
+        userdata[username] = {}; //entered username replaces 'username' in json file
+        userdata[username].name = request.body.name; //supplies name to be set to 'name' in json file
+        userdata[username].password = request.body.password; //supplies password to be set to 'password' in json file
+        userdata[username].email = request.body.email; //supplies email to be set to 'email' in json file
 
-    //set the below variables to what was input by the user on the page
-    userdata[username] = {}; //entered username replaces 'username' in json file
-    userdata[username].name = request.body.name; //supplies name to be set to 'name' in json file
-    userdata[username].password = request.body.password; //supplies password to be set to 'password' in json file
-    userdata[username].email = request.body.email; //supplies email to be set to 'email' in json file
-
-    if (errs.length == 0) { //if there are no errors...
+        //if there are no errors...
         fs.writeFileSync(user_info_file, JSON.stringify(userdata, null, 2));//input the above fields filled out by user into the user_data.json file, using 'null, 2' to format the json file with 2 spaces as an indent between objects
-        const successful_register_stringified = queryString.stringify(request.query); //stringify query
-        response.redirect("./invoice.html?" + successful_register_stringified); //redirect user to invoice with newly created account
+        const registration_stringified = queryString.stringify(request.query); //stringify query
+        response.redirect("./invoice.html?" + registration_stringified); //redirect user to invoice with newly created account
     } else {
         response.end(JSON.stringify(errs)); //otherwise, show error message in query string
     }
